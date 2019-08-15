add50b3af748e1fccf9b016d0c3193276119f775
Bob Weilbaecher <rweilbae@uno.edu>
2019-08-15 13:14:32 -0500

    first commit - completed project

---
+++ backend/routes/users.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+let User = require("../models/user.model");
+
+router.route("/").get((req, res) => {
+  console.log("users/ route hit");
+  User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.route("/add").post((req, res) => {
+  console.log("users/add route hit");
+  console.log("req.body", req.body);
+  const username = req.body.username;
+  console.log("username: ", username);
+
+  const newUser = new User({ username });
+
+  newUser
+    .save()
+    .then(() => res.json("User added!"))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
